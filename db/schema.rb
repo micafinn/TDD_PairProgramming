fef585188eaac96c68aaf52d179f2eb941ec84b5
micafinn <micafinn@gmail.com>
2017-04-20 10:35:08 +0200

    Add title and body to post, add validation to post, add title and body to fixtures

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420075234) do
+ActiveRecord::Schema.define(version: 20170420081811) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "body"
   end
 
 end
