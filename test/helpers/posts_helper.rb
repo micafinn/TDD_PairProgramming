46b1dcb1e02041b3853aaaea99abb86082cebef9
micafinn <micafinn@gmail.com>
2017-04-21 11:37:38 +0200

    Define the links_to_post and post a fixture in order to test it

--- test/helpers/posts_helper.rb
+++ test/helpers/posts_helper.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
-class PostHelperTest < ActionView::TestCase
+class PostsHelperTest < ActionView::TestCase
   test "link_to_post helper should get correct path" do
     post = posts(:one)
-    assert_dom_equal %{<a href="/posts/#{post.id}">title_string</a> }, link_to_post(post)
-
+    assert_dom_equal %{<a href="/posts/#{post.id}">TitleString</a>}, link_to_post(post)
   end
 end
